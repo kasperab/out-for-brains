9ed200379088f94c8f6cc95e73d622e62bf910e5
kasperab <kasperab@yahoo.se>
2023-03-26 15:25:38 +0200

    Created system for switching camera when player enters trigger

--- Out for Brains/Assets/Scripts/CameraHandler.cs
+++ Out for Brains/Assets/Scripts/CameraHandler.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
 public class CameraHandler : MonoBehaviour
 {
-    public Camera currentCamera;
+	public static Camera currentCamera = null;
+	public new Camera camera;
+	public bool startCamera = false;
+
+	private void Start()
+	{
+		if (startCamera && !currentCamera)
+		{
+			camera.enabled = true;
+			currentCamera = camera;
+		}
+		else
+		{
+			camera.enabled = false;
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.GetComponent<NavMeshAgent>())
+		{
+			currentCamera.enabled = false;
+			camera.enabled = true;
+			currentCamera = camera;
+		}
+	}
 }
--- Out for Brains/Assets/Scripts/Player.cs
+++ Out for Brains/Assets/Scripts/Player.cs
@@ -5,19 +5,18 @@ using UnityEngine.AI;
 [RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
 {
-	public CameraHandler cameraHandler;
 	private NavMeshAgent agent;
 	private Animator animator;
 
-	void Start()
+	private void Start()
 	{
 		agent = GetComponent<NavMeshAgent>();
 		animator = GetComponent<Animator>();
 	}
 
-	void Update()
+	private void Update()
 	{
-		Ray ray = cameraHandler.currentCamera.ScreenPointToRay(Input.mousePosition);
+		Ray ray = CameraHandler.currentCamera.ScreenPointToRay(Input.mousePosition);
 		if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
 		{
 			if (Input.GetMouseButtonDown(0))
